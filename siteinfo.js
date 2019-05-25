b4c2b4204d305a814415f7bd8784882f86080492
fkj <dashen_mr.f@aliyun.com>
2019-05-25 20:43:36 +0800

    Initial Commit

---
+++ siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "志汇-外卖点餐多店营销版",
+    uniacid: "4",
+    acid: "2",
+    multiid: "0",
+    version: "9.4.0",
+    siteroot: "https://fankangjia.top/app/index.php",
+    design_method: "3"
+};
